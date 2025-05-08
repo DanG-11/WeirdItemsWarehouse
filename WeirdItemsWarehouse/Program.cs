2eef1c00c5922b27297e48d70fac57122d11e204
DanG-11 <daniil.gavrilov@teb.edu.pl>
2025-05-09 00:23:42 +0200

    Stworzenie struktury, konstruktora oraz opisu klasy item.

--- WeirdItemsWarehouse/Program.cs
+++ WeirdItemsWarehouse/Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace WeirdItemsWarehouse
 {
+    class Item
+    {
+        public string name { get; set; }
+        public double weight_kg { get; set; }
+        public int weirdness_level { get; set; }
+        public bool is_fragile { get; set; }
+
+        public Item(string Name, double Weight_kg, int Weirdness_level, bool Is_fragile)
+        {
+            name = Name;
+            weight_kg = Weight_kg;
+            weirdness_level = Weirdness_level;
+            is_fragile = Is_fragile;
+        }
+
+        public string Description()
+        {
+            return "{\n\t Item description:" +
+                   $"\t\"name\": \"{name}\",\n" +
+                   $"\t\"weight_kg\": \"{weight_kg}\",\n" +
+                   $"\t\"weirdness_level\": \"{weirdness_level}\",\n" +
+                   $"\t\"is_fragile\": \"{(is_fragile ? "YES" : "NO")}\",\n" +
+                   "}";
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
         }
     }
 }
